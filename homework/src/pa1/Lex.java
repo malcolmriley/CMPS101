1f44eac73d413b6cc71bd1345dc9b7bcae9d5140
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-07 18:06:22 -0700

    reader implemented

--- homework/src/pa1/Lex.java
+++ homework/src/pa1/Lex.java
@@ -8,11 +8,24 @@
  *********************************************************************/
 package pa1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Lex {
 
 	public static void main(String[] passedArguments) {
 		if (passedArguments.length < 2) {
-			System.err.println("Fewer than two arguments passed to command. Usage: ");
+			System.err.println("Fewer than two arguments provided. Usage: [inputfile] [outputfile]");
+		}
+		else {
+			try (BufferedReader reader = new BufferedReader(new FileReader(passedArguments[0]))) {
+				ArrayList<String> stringArray = new ArrayList<String>();
+				reader.lines().forEachOrdered(stringArray::add);
+			} catch (IOException passedException) {
+				passedException.printStackTrace();
+			}
 		}
 	}
 
