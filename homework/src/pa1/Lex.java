8c532bfa3da9b0c2b179bb4e1f40ce7cdd5a9b6d
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-07 17:51:42 -0700

    File comments

--- homework/src/pa1/Lex.java
+++ homework/src/pa1/Lex.java
@@ -1,9 +1,19 @@
+/*********************************************************************
+ * Malcolm Riley
+ * 
+ * CruzID: masriley
+ * Assignment: pa1
+ * 
+ * 10-2017
+ *********************************************************************/
 package pa1;
 
 public class Lex {
 
 	public static void main(String[] passedArguments) {
-		
+		if (passedArguments.length < 2) {
+			System.err.println("Fewer than two arguments passed to command. Usage: ");
+		}
 	}
 
 }
