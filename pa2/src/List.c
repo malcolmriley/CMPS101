92903980dc22a39951d88c7dfdeb1a8c3e018cc7
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-15 16:11:20 -0700

    First steps in List

--- pa2/src/List.c
+++ pa2/src/List.c
@@ -9,3 +9,29 @@
 
 #include "List.h"
 
+/* Node Type Definition */
+typedef struct NodeObj {
+	int value;
+	struct NodeObj* nextNode;
+	struct NodeObj* previousNode;
+} NodeObj;
+typedef NodeObj* Node;
+
+Node newNode(int passedValue) {
+
+}
+void freeNode(Node* passedNode) {
+
+}
+
+/* List Globals */
+
+/* List Function Definitions */
+
+/* Internal Functions */
+void checknull(void* passedPointer, char* passedCharArray) {
+	if (passedPointer == NULL) {
+		printf("Null pointer exception: List is null.\n %s", passedCharArray);
+		exit(1);
+	}
+}
