02150a1d95cf3d20f2bf8ddf6418a0b0221f9d97
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-10-15 14:43:29 -0700

    Add Lex.c

---
+++ pa2/src/Lex.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : pa2.c
+ Author      : Malcolm Riley
+ Version     :
+ Copyright   : No copyright!
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
