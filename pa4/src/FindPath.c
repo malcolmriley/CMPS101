05eb38400202afb129511c72f79ee2454aa1e4be
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-13 07:16:54 -0800

    Initial Commit

---
+++ pa4/src/FindPath.c
@@ -0,0 +1,16 @@
+/*********************************************************************
+ * Malcolm Riley
+ *
+ * CruzID: masriley
+ * Assignment: pa4
+ *
+ * 11-2017
+ *********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	return EXIT_SUCCESS;
+}
