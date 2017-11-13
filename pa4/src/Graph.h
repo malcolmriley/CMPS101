9377a329b4bc53bde5ff965ece2c5fd4a0902d23
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-13 07:25:50 -0800

    Fix comments

--- pa4/src/Graph.h
+++ pa4/src/Graph.h
@@ -17,11 +17,11 @@ typedef struct Graph {
 
 } Graph;
 
-/*** Constructors-Destructors ***/
+/* Constructors-Destructors */
 Graph newGraph(int n);
 void freeGraph(Graph* pG);
 
-/*** Access functions ***/
+/* Accessors */
 int getOrder(Graph G);
 int getSize(Graph G);
 int getSource(Graph G);
@@ -29,13 +29,13 @@ int getParent(Graph G, int u);
 int getDist(Graph G, int u);
 void getPath(List L, Graph G, int u);
 
-/*** Manipulation procedures ***/
+/* Manipulatiors */
 void makeNull(Graph G);
 void addEdge(Graph G, int u, int v);
 void addArc(Graph G, int u, int v);
 void BFS(Graph G, int s);
 
-/*** Other operations ***/
+/* Miscellaneous */
 void printGraph(FILE* out, Graph G);
 
-#endif /* GRAPH */
+#endif
