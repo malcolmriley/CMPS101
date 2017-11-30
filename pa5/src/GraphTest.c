2ede5190462956fd87332fa329333573234717c5
Malcolm Riley <malcolmriley@users.noreply.github.com>
2017-11-29 16:40:51 -0800

    Update comment header

--- pa5/src/GraphTest.c
+++ pa5/src/GraphTest.c
@@ -2,7 +2,7 @@
  * Malcolm Riley
  *
  * CruzID: masriley
- * Assignment: pa4
+ * Assignment: pa5, modified from pa4
  *
  * 11-2017
  *********************************************************************/
