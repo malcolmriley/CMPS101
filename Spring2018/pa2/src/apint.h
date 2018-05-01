a3ad95da2eaca3a920ab9d6ad85c2c9d6f9740a7
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-01 12:52:02 -0700

    Initial commit for apint.h

---
+++ Spring2018/pa2/src/apint.h
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * PA2: Arbitrary Precision Integer, in C.
+ *
+ * Malcolm Riley
+ * CMPS 101 Spring 2018
+ ******************************************************************************/
+
+#ifndef APINT_H
+#define APINT_H
+
+typedef struct apint_object {
+	int SIZE;
+	int VALUE[];
+	int CARRY[];
+} apint_object;
+typedef apint_object* apint;
+
+/* Constructors - Destructor */
+apint newApint(void);
+apint newApint(int);
+apint newApint(char*);
+void freeApint(apint*);
+
+/* Accessors */
+int equals(apint);
+int compare(apint);
+
+/* Manipulators */
+apint add(apint, apint);
+apint subtract(apint, apint);
+apint multiply(apint, apint);
+
+/* Miscellaneous */
+void print(FILE*, apint);
+apint copy(apint);
+
+#endif /* APINT_H */
