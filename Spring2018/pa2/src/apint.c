a46c3436d1eaa4e0c532c9575df34fb5853324bc
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-01 14:25:31 -0700

    Add method skeletons

---
+++ Spring2018/pa2/src/apint.c
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * PA2: Arbitrary Precision Integer, in C.
+ *
+ * Malcolm Riley
+ * CMPS 101 Spring 2018
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "apint.h"
+
+/* Internal Function Declarations */
+int checkSize(apint, int);
+int get(apint, int);
+void set(apint, int);
+void expand(apint, int);
+void zero(apint);
+
+/* Header-Defined Functions */
+
+/**
+ * Default Constructor.
+ */
+apint newApint(void) {
+	apint instance = malloc(sizeof(apint_object));
+	expand(instance, 2);
+	zero(instance);
+	return instance;
+}
+
+/**
+ * Constructor for conversion of an int to an apint.
+ */
+apint newApint(int passedValue) {
+	apint instance = newApint();
+	set(instance, 0, passedValue);
+	return instance;
+}
+
+/**
+ * Constructor for conversion of a char array (string) to an apint.
+ */
+apint newApint(char* passedArray) {
+
+}
+
+/* Frees the passed apint */
+void freeApint(apint* passedApint) {
+	free(*(passedApint->CARRY));
+	free(*(passedApint->VALUE));
+	free(*passedApint);
+}
+
+/**
+ * Compares the two passed apints for equality;
+ *
+ * Returns 0 if passedFirst = passedSecond, 1 otherwise.
+ */
+int equals(apint passedFirst, apint passedSecond) {
+	return (compare(passedFirst, passedSecond)) == 0;
+}
+
+/**
+ * Compares the two passed apints.
+ *
+ * Returns 0 if they are equal, 1 if passedFirst > passedSecond, and -1 if passedFirst < passedSecond.
+ */
+int compare(apint passedFirst, apint passedSecond) {
+	// TODO:
+}
+
+/**
+ * Adds (passedFirst + passedSecond), returning the result as a new apint.
+ */
+apint add(apint passedFirst, apint passedSecond) {
+	// TODO:
+}
+
+/**
+ * Subtracts (passedFirst - passedSecond), returning the result as a new apint.
+ */
+apint subtract(apint passedFirst, apint passedSecond) {
+	// TODO:
+}
+
+/**
+ * Multiplies (passedFirst * passedSecond), returning the result as a new apint.
+ */
+apint multiply(apint passedFirst, apint passedSecond) {
+	// TODO:
+}
+
+/**
+ * Prints the passed apint to the passed FILE.
+ */
+void print(FILE* passedFile, apint passedValue) {
+	// TODO:
+}
+
+/* Internal Methods */
+
+int checkSize(apint passedApint, int passedSize) {
+	return (passedSize > 0) || (passedSize < passedApint->SIZE);
+}
+
+/**
+ * Sets the value at passedIndex to passedValue, expanding passedApint if necessary.
+ */
+void set(apint passedApint, int passedIndex, int passedValue) {
+	if(!checkSize(passedApint, passedIndex)) {
+		expand(passedApint, passedIndex);
+	}
+	passedApint->VALUE[passedIndex] = passedValue;
+}
+
+/**
+ * Safely returns the int value stored at passedIndex in passedApint.
+ *
+ * Returns 0 if passedIndex is out of range, and the requested value otherwise.
+ */
+int get(apint passedApint, int passedIndex) {
+	if (checkSize(passedApint, passedIndex)) {
+		return passedApint->VALUE[passedIndex];
+	}
+	return 0;
+}
+
+/**
+ * Expands passedApint to accomodate passedSize.
+ */
+void expand(apint passedApint, int passedSize) {
+	// TODO:
+	passedApint->SIZE = passedSize;
+}
+
+/**
+ * Zeroes the passed apint.
+ */
+void zero(apint passedApint) {
+	passedApint->SIGN = 0;
+	for (int index = 0; index < passedApint->SIZE; index += 1) {
+		passedApint->VALUE[index] = 0;
+		passedApint->CARRY[index] = 0;
+	}
+}
