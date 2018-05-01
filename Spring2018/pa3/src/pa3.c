c60f186f4d542da1478de139c948161691175f87
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-01 03:25:10 -0700

    Move to appropriate folder

---
+++ Spring2018/pa3/src/pa3.c
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * Name        : pa3.c
+ * Author      : Malcolm Riley
+ * Version     :
+ * Copyright   : CMPS 101 Spring 2018
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
