5c8d0db7219bf14205b0602a3bd9329d85c0bff9
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-06 22:45:41 -0700

    Add method comments

--- pa3/src/pa3/Anagram.java
+++ pa3/src/pa3/Anagram.java
@@ -2,37 +2,71 @@ package pa3;
 
 /**
  * Implementation for {@link Anagram} type as per assignment requirements: 
- * <li> a constructor which uses a String, made up of alphabetic characters either upper or lower case as an input argument. </li>
- * <li> a constructor which uses a char array, made up of alphabetic characters either upper or lower case as an input argument. </li>
- * <li> a method for printing. </li>
- * <li> a method for comparing two Anagram variables that returns true or false. </li>
- * <li> a method that returns the word part of an Anagram variable. </li>
- * <li> do not allow user to get the code part of an Anagram variable. </li>
+ * <li> 1) a constructor which uses a String, made up of alphabetic characters either upper or lower case as an input argument. </li>
+ * <li> 2) a constructor which uses a char array, made up of alphabetic characters either upper or lower case as an input argument. </li>
+ * <li> 3) a method for printing. </li>
+ * <li> 4) a method for comparing two Anagram variables that returns true or false. </li>
+ * <li> 5) a method that returns the word part of an Anagram variable. </li>
+ * <li> 6) do not allow user to get the code part of an Anagram variable. </li>
  * 
  * @author Malcolm Riley 2018
  */
 public class Anagram {
 	
+	/**
+	 * Constructor for a new {@link Anagram} instance.
+	 * <p>
+	 * Fulfills requirement #1.
+	 * 
+	 * @param passedString - The {@code String} to use for the {@link Anagram}.
+	 */
 	public Anagram(String passedString) {
 		this(passedString.toCharArray());
 	}
 	
+	/**
+	 * Constructor for a new {@link Anagram} instance.
+	 * <p>
+	 * Fulfills requirement #2.
+	 * 
+	 * @param passedArray - The {@code char} array to use for the {@link Anagram}.
+	 */
 	public Anagram(char[] passedArray) {
 		// TODO
 	}
 	
+	/**
+	 * Print method.
+	 * <p>
+	 * Fulfills requirement #3.
+	 */
 	public void print() {
-		// TODO
+		System.out.println(this.getWord());
 	}
 	
-	public String getWord() {
-		// TODO
-		return null;
-	}
-	
+	/**
+	 * Method for directly comparing two {@link Anagram} instances.
+	 * <p>
+	 * Fulfills requirement #4.
+	 * 
+	 * @param passedAnagram - The {@link Anagram} to compare against
+	 * @return Whether or not the two {@link Anagram} instances' backing words are anagrams.
+	 */
 	public boolean areAnagrams(Anagram passedAnagram) {
 		// TODO
 		return false;
+	}
+	
+	/**
+	 * Method returning the "word" part of an {@link Anagram}.
+	 * <p>
+	 * Fulfills requirement #5.
+	 * 
+	 * @return The {@code String} word backing this {@link Anagram}.
+	 */
+	public String getWord() {
+		// TODO
+		return null;
 	}
 	
 	/* Internal Methods */
